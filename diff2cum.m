d3736e6f4038704809b33e33a99add8bd78131e5
SKLEC-LAOLee <whli@sklec.ecnu.edu.cn>
2023-10-26 13:35:55 +0800

    Update diff2cum.m

--- diff2cum.m
+++ diff2cum.m
@@ -1,4 +1,4 @@
-function [channelUpperSize,cumuPercent,medianVal,meanVal,passingVal]=diff2cum(indexVar,diffPercent,rangeOfIndexVar,userPercentLevel)
+function [channelUpperSize,cumuPercent,medianVal,meanVal,passingVal]=diff2cum(indexVar,diffPercentage,rangeOfIndexVar,userPercentLevel)
 %----------------------------------------------------------------------------------------------------
 % @file name:   diff2cum.m
 % @description: calculate the cumulative percentage from differential data
@@ -7,7 +7,7 @@ function [channelUpperSize,cumuPercent,medianVal,meanVal,passingVal]=diff2cum(in
 %----------------------------------------------------------------------------------------------------
 % @param:
 % indexVar, the variable being counted, for example: grainsize, shape factor, age......
-% diffPercent, channel content. Channel width considered as half-logarithmic width on both side, %
+% diffPercentage, channel content. Channel width considered as half-logarithmic width on both side, percentage
 % rangeOfIndexVar, minimun and maximum index size, used for evaluating the lower/upper limit of the minimum/maximum channel
 % userPercentLevel, the percent level to find the passing value of indexVar
 % @return:
@@ -22,10 +22,10 @@ function [channelUpperSize,cumuPercent,medianVal,meanVal,passingVal]=diff2cum(in
 % discard invalid value
 validValId=(indexVar>=rangeOfIndexVar(1))&(indexVar<=rangeOfIndexVar(2));
 indexVar=indexVar(validValId);
-diffPercent=diffPercent(validValId);
+diffPercentage=diffPercentage(validValId);
 % order the variable to make it monotonically increasing
 [newIndexVar,iRaw]=sort(indexVar);
-newDiffPercent=diffPercent(iRaw);
+newDiffPercentage=diffPercentage(iRaw);
 middleSizeAndMaxLim=unique([newIndexVar(:);rangeOfIndexVar(2)]);
 
 tempVar=log2(middleSizeAndMaxLim-min(middleSizeAndMaxLim)+1e-7);
@@ -38,11 +38,11 @@ cumuPercent=zeros(nChannel,1);
 %medianVal=nan;
 for iChannel=1:nChannel
     passingId=newIndexVar<=channelUpperSize(iChannel,1);
-    cumuPercent(iChannel,1)=sum(newDiffPercent(passingId));
+    cumuPercent(iChannel,1)=sum(newDiffPercentage(passingId));
 end
 %channelLowerSize(1)=rangeOfIndexVar(1);
 %channelLowerSize(2:end)=channelUpperSize(1:end-1);
-meanVal=sum(indexVar.*diffPercent)./sum(diffPercent);
+meanVal=sum(indexVar.*diffPercentage)./sum(diffPercentage);
 % make the variable monotonically increasing
 uniqueCumuPercent=cumuPercent(:)+(1:nChannel)'.*1e-7;
 medianVal=interp1(uniqueCumuPercent,channelUpperSize,50);

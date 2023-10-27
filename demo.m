2bd9f4a2252b97346f4d452b33e942e67b084638
SKLEC-LAOLee <whli@sklec.ecnu.edu.cn>
2023-10-27 14:51:16 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -4,12 +4,12 @@
 % @author:      Li Weihua, whli@sklec.ecnu.edu.cn
 % @version:     Ver1.0, 2023.10.26
 %----------------------------------------------------------------------------------------------------
-userSettings.sampleSettingFileName='.\testData\camSizerX2\sampleSettings.txt'; %sample settings information file
+userSettings.sampleSettingFileName='.\testData\camSizerX2\sampleSettings.csv'; %sample settings information file
 userSettings.dataPath='.\testData\camSizerX2\';                     %full path of the raw data files
-userSettings.outputPath='E:\test\';                                 %full path of the output files
-userSettings.prefixString='testCamSizer';                           %prefixes for archive file names
+userSettings.outputPath='.\testData\camSizerX2\outputs\';           %full path of the output files
+userSettings.prefixString='test';                                   %prefixes for archive file names
 userSettings.forceReadRawData=true;                                 %= true, allways read data from raw files;= false, load the rawData.mat if exists in the dataPath; otherwise, read data from raw files
-userSettings.MIN_CHANNEL_SIZE_UM=0.01;                              %lower limit of instrument detection (um), should be greater than 0, default is 0.01um
+userSettings.MIN_CHANNEL_SIZE_UM=0.05;                              %lower limit of instrument detection (um), should be greater than 0, default is 0.001um
 userSettings.MAX_CHANNEL_SIZE_UM=1e4;                               %upper limit of instrument detection (um), default is 10mm
 userSettings.GradationCurveFigWidth=7.13;                           %figure width of the gradation curve, in unit of cm
 userSettings.GradationCurveFigHeight=6;                             %figure height of the gradation curve, in unit of cm
